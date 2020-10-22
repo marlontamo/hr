1b00d0fcf52b7fef58161e1cd8464b42d94e7c55
marlontamo <marlontamo@gmail.com>
2020-10-22 14:36:28 +0800

    2nd day modifying parse error json< at index 0

---
+++ apps/config/default/users/1.php
@@ -0,0 +1,15 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['user']['user_id'] = 1;
+$config['user']['role_id'] = 1;
+$config['user']['sensitivity'] = '';
+$config['user']['region_companies'] = '';
+$config['user']['language'] = 'en';
+$config['user']['username'] = 'loonie';
+$config['user']['email'] = 'marlontamo@gmail.com';
+$config['user']['firstname'] = "";
+$config['user']['lastname'] = "";
+$config['user']['middlename'] = "";
+$config['user']['suffix'] = "";
+$config['user']['photo'] = '';
+$config['user']['timezone'] = 'Asia/Manila';
